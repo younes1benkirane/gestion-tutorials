9ae3f28510f33641b7430b131477355ddf7035db
younes1benkirane@gmail.com <Barcamessi2008>
2024-09-23 10:23:44 +0200

    intial commit

---
+++ gestion-tutoriels/src/main/java/com/younes/gestion_tutoriels/back/GestionTutorielsApplication.java
@@ -0,0 +1,13 @@
+package com.younes.gestion_tutoriels.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTutorielsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTutorielsApplication.class, args);
+	}
+
+}
---
+++ gestion-tutoriels/src/test/java/com/younes/gestion_tutoriels/back/GestionTutorielsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younes.gestion_tutoriels.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTutorielsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
